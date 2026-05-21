2612f1258259048bd61d6fe4178b34b411517de5
Satoukk <gotokouzou312@icloud.com>
2026-05-21 11:42:41 +0900

    "データベース設定"

---
+++ backend/laravel/app/Http/Controllers/ScheduleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateScheduleRequest;
+use App\Http\Requests\UpdateScheduleRequest;
+use App\Models\Schedules;
+use Illuminate\Http\JsonResponse;
+
+class ScheduleController extends Controller
+{
+    
+}
